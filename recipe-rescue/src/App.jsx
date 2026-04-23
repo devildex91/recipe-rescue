011f2a6a10e0a9af20c943a2972199f285916b7f
Neall Dexter <neall.t.dexter@gmail.com>
2026-04-23 17:49:52 +0100

    navbar section and title added as well as logo and background image

--- recipe-rescue/src/App.jsx
+++ recipe-rescue/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 
 import './App.css'
+import Navbar from  "./components/Navbar"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <h1>Lets get this working</h1>
+    <Navbar />
     </>
   )
 }
---
+++ recipe-rescue/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import logo from "../assets/images/recipe-rescue-logo.png"
+
+export default function Navbar(){
+
+return (
+<nav>
+    
+    
+    <h1><img id="logo" src={logo}/></h1>
+    
+</nav>
+
+)
+
+}
