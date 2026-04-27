e353b6d7307d95d7d96bc9ea663429e17935af99
Neall Dexter <neall.t.dexter@gmail.com>
2026-04-27 19:43:40 +0100

    onClick added to extract recipe id ready to render full recipe on click

--- recipe-rescue/src/spoonRecipe.jsx
+++ recipe-rescue/src/spoonRecipe.jsx
@@ -6,6 +6,29 @@ export default function SpoonRecipe(props) {
   //state for recipe storage
   const [recipes, setRecipes] = useState([]);
   const ingredientList = props.ingredients.join(",");
+  //state for storing full recipe
+const [fullRecipe, setFullRecipe] = useState([])
+//
+ function recipeDetail(recipeId){
+   useEffect(() => {
+
+
+    fetch(
+      `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredientList}&number=6&apiKey=${API}`,
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        setRecipes(result);
+      })
+      .catch((err) => console.error("Fetch error:", err));
+  }, [ingredientList, API]);
+  
+
+  
+  console.log(recipeId)
+
+
+ }
 
   useEffect(() => {
     if (props.ingredients.length === 0) return;
@@ -20,12 +43,14 @@ export default function SpoonRecipe(props) {
       .catch((err) => console.error("Fetch error:", err));
   }, [ingredientList, API]);
 
+  
+
   const recommendedRecipes = recipes.map((recipe) => (
-    <button className="unbutton" onClick={recipeDetail}>
-      <div className="recipeCard shadow" key={recipe.id}>
-        <h3>{recipe.title}</h3>
-        <img src={recipe.image} alt={recipe.title} />
-        <div className="ingredients missing">
+    <button className="unbutton"key={recipe.id} onClick={() => recipeDetail(recipe.id)}>
+    <div className="recipeCard shadow" key={recipe.id}>
+      <h3>{recipe.title}</h3>
+      <img src={recipe.image} alt={recipe.title} />
+      <div className="ingredients missing">
           <h4>missing ingredients</h4>
           <ul>
             {recipe.missedIngredients.map((ingredients) => (
@@ -33,8 +58,13 @@ export default function SpoonRecipe(props) {
             ))}
           </ul>
         </div>
-      </div>
+    </div>
     </button>
   ));
-  return <section id="recipeResult">{recommendedRecipes}</section>;
+  return (
+
+    <section id="recipeResult">
+      {recommendedRecipes}
+    </section>
+  );
 }
