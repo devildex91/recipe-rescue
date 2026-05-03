9722ffb58c5a68655adc21a77660047c86ce4a3a
Neall Dexter <neall.t.dexter@gmail.com>
2026-05-03 17:11:19 +0100

    opening paragraph reworded

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,10 +8,9 @@ const screenWidth = window.innerWidth > 768;
       <div id="textDiv">
       <h2>Welcome to Recipe Rescue</h2>
       <p>
-        Our aim is to stop food waste one ingredient it a time, How? you might
-        ask. Well you provide us with the ingredients (at least 5 please. We are good but we cant work miracles) and we provide the recipe.
-        It really is that simple so if your ready to go lets stop the waste and
-        lets start the taste.
+        Our aim is to stop food waste one ingredient at a time. How you might
+        ask? Well, you provide us with the ingredients (at least 5 please-we're good but we can't work miracles!), and we provide the recipe.
+        It really is that simple. So if you're ready to go lets stop the waste and start the taste.
       </p>
       </div>
       <div id="headerImg">
